33e13825a395e2eaaaa60a14d843bfac5f71a65d
Cooper Duan <cooperd@synnex.com>
2021-03-27 15:11:37 +0800

    feat: use vite

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import Demo1 from './components/chapter1';
+import Demo2 from './components/chapter2';
+import Demo4 from './components/chapter4';
+
+export default Demo4;
---
+++ src/components/chapter3/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+     ssr starter
+    </div>
+  );
+};
+
+export default App;
